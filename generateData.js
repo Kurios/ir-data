344fbff78bf16afac51552bf4a318fc7289020cc
kurios <kuriosly@gmail.com>
2021-12-11 15:45:08 -0800

    more changes...

---
+++ generateData.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+import http from 'http'
+
+
+const db = (async () => {
+    // open the database
+    open({
+        filename: 'database.db',
+        driver: sqlite3.Database
+    }).then( async (db)=> {
+
+    db.on("trace", (err)=>{console.log(err)});
+    
+    let server = http.createServer(async (req,res)=>{
+        res.writeHead(200, {'Content-Type':"text/json", 'Access-Control-Allow-Origin':'*'})
+        //res.write("name prosperity timestamp")
+
+        const result = await db.all('SELECT unions.name, prosperity, datetime FROM prosperity INNER JOIN unions ON unions.id = prosperity.unionid')
+        for (let item of result) {
+            item.datetime = Math.round(new Date(item.datetime).getTime())
+        }
+        res.write(JSON.stringify(result))
+        res.end()
+    })
+    
+    server.listen(9898);
+    console.log("server up at localhost:9898")
+
+
+    })})();
